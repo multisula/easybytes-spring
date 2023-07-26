50447206774c8bd0dbe596e38da6e59f745b72a7
multisula <multisula@gmail.com>
2023-07-26 21:46:28 +0900

    example6

---
+++ example6/src/main/java/com/example/beans/Vehicle.java
@@ -0,0 +1,27 @@
+package com.example.beans;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Vehicle {
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @PostConstruct
+  public void initialize() {
+    this.name = "Honda";
+  }
+
+  public void printHello() {
+    System.out.println(
+        "Printing Hello from component Vehicle Bean");
+  }
+}
